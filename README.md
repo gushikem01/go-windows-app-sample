aca49f5a149db6c0881bde4290d41edddb3f7113
gushikem01 <gl_higaisha@yahoo.co.jp>
2022-10-31 14:47:25 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+## develop golang desktop application on WSL2
+
+* Install Xlaunch in you PC
+
+<https://dev.classmethod.jp/articles/wsl-x-window/>
+
+* Add dependency to your project
+
+```bash
+go install -x -v github.com/go-delve/delve/cmd/dlv@v1.7.1-0.20210804080032-f95340ae1bf9
+sudo apt install -y libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libxxf86vm-dev
+```
+
+<https://blog.odaryo.com/2020/01/wsl2-xserver-export-display/>
+
+* .vscode/launch.json
+
+```json
+  "env": {
+        "DISPLAY": "(YOURIPADDRESS):0.0",
+```
+
+* export DISPLAY
+
+```bash
+export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0
+```
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
+)
+
+func main() {
+
+	mainApp := app.New()
+	env := os.Getenv("DISPLAY")
+	fmt.Println("DISPLAY:", env)
+
+	w := mainApp.NewWindow("Sample")
+	w.Resize(fyne.NewSize(200, 30))
+
+	w.SetContent(
+		fyne.NewContainerWithLayout(
+			layout.NewVBoxLayout(),
+			widget.NewButton("Counter", func() { newCounterWindow(mainApp).Show() }),
+		),
+	)
+	w.ShowAndRun()
+}
+
+type counter struct {
+	*widget.Label
+	*widget.Button
+	count int
+}
+
+func newCounter() *counter {
+	c := &counter{}
+	c.Label = widget.NewLabel(fmt.Sprintf("Count: %d", c.count))
+	c.Button = widget.NewButton("Click!", c.countUp)
+	return c
+}
+
+func (c *counter) countUp() {
+	c.count++
+	c.Label.SetText(fmt.Sprintf("Count: %d", c.count))
+}
+
+type counterWindow struct {
+	fyne.Window
+	*counter
+}
+
+func newCounterWindow(app fyne.App) *counterWindow {
+	counter := newCounter()
+	w := app.NewWindow("Counter")
+	w.Resize(fyne.NewSize(200, 30))
+	w.SetContent(
+		fyne.NewContainerWithLayout(
+			layout.NewVBoxLayout(),
+			counter.Button, counter.Label,
+		),
+	)
+	return &counterWindow{w, counter}
+}
